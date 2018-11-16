9244b07c0b6d34ad22e522bf2e0eb390e1ad06e8
Wagram Airiian <vagram.airiian@tum.de>
2018-11-16 23:04:05 +0100

    Initialised with an app

---
+++ carme/carme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  carme
+//
+//  Created by Wagram Airian on 16.11.18.
+//  Copyright © 2018 Wagram Airian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
